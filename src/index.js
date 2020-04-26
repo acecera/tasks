55e754f358a829cdc9538e07818652c741153f87
Anthony Cera <ace_cera@yahoo.com>
2020-04-25 18:47:07 -0700

    added some middleware to the tasks so that I could have a task be unique to each owner that created it .

--- src/index.js
+++ src/index.js
@@ -25,3 +25,18 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is listening on port ' + port)
 })
+
+// const Task = require('./models/task')
+// const User = require('./models/user')
+
+const main  = async () => {
+    // const task = await Task.findById('')
+    // await task.populate('owner').execPopulate()
+    // console.log(task.owner)
+
+    // const user = await User.findById('')
+    // await user.populate('tasks').execPopulate()
+    //console.log(user.tasks)
+}
+
+main ()
