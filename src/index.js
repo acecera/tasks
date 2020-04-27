fb309ac7d2e6891a86bb7c6efaa69d781ed9d4b1
Anthony Cera <ace_cera@yahoo.com>
2020-04-26 17:03:45 -0700

    added some pagination to the get tasks call. Added multer middleware package to the users model.

--- src/index.js
+++ src/index.js
@@ -6,17 +6,25 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000 
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('Get requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
+const multer = require('multer')
+const upload = multer({
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    }, 
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Please upload a Word Doc'))
+        }
 
-// app.use((req, res, next) => {
-//     res.status(503).send('Site is currently down. Check back soon!')
-// })
+        cb(undefined, true)
+    }
+})
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
 
 app.use(express.json())
 app.use(userRouter)
@@ -25,18 +33,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is listening on port ' + port)
 })
-
-// const Task = require('./models/task')
-// const User = require('./models/user')
-
-const main  = async () => {
-    // const task = await Task.findById('')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
-
-    // const user = await User.findById('')
-    // await user.populate('tasks').execPopulate()
-    //console.log(user.tasks)
-}
-
-main ()
