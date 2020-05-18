60bda1be1c4c99cc739a4d86d581a94e3c97fcf6
Anthony Cera <ace_cera@yahoo.com>
2020-05-17 18:27:41 -0700

    added some testing for tasks. added a db file to house the model syntax for users and tasks. added --runInBand to test script in package.json.

---
+++ tests/fixtures/db.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const User = require('../../src/models/user')
+const Task = require('../../src/models/task')
+
+
+const userOneId = new mongoose.Types.ObjectId()
+const userOne = {
+    id: userOneId,
+    name: 'Tyler',
+    email: 'tyler@example.com',
+    password: 'notarealone',
+    tokens: [{
+        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
+    }]
+}
+
+const userTwoId = new mongoose.Types.ObjectId()
+const userTwo = {
+    id: userTwoId,
+    name: 'Anthony',
+    email: 'anthony@example.com',
+    password: 'passthisonenow',
+    tokens: [{
+        token: jwt.sign({ _id: userTwoId }, process.env.JWT_SECRET)
+    }]
+}
+
+const taskOne = {
+    _id: mongoose.Types.ObjectId(),
+    description: 'first task',
+    completed: false,
+    owner: userOne._id
+}
+
+const taskTwo = {
+    _id: mongoose.Types.ObjectId(),
+    description: 'second task',
+    completed: false,
+    owner: userOne._id
+}
+
+const taskThree = {
+    _id: mongoose.Types.ObjectId(),
+    description: 'third task',
+    completed: false,
+    owner: userTwo._id
+}
+
+const setupDatabase = async () => {
+    await User.deleteMany()
+    await Task.deleteMany()
+    await new User(userOne).save()
+    await new User(userTwo).save()
+    await new Task(taskOne).save()
+    await new Task(taskTwo).save()
+    await new Task(taskThree).save()
+}
+
+module.exports = {
+    userOneId, 
+    userOne,
+    userTwoId,
+    userTwo,
+    taskOne,
+    taskTwo,
+    taskThree,
+    setupDatabase
+}
